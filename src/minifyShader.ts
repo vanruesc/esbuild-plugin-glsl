88951f061c6008a1df46ca191ea636aee44b5bde
Raoul v. R <vanruesc@users.noreply.github.com>
2020-12-08 11:31:29 +0100

    Add credits

--- src/minifyShader.ts
+++ src/minifyShader.ts
@@ -1,6 +1,8 @@
 /**
  * Minifies the given GLSL source code.
  *
+ * Based on https://github.com/vwochnik/rollup-plugin-glsl
+ *
  * @param source The source code.
  * @return The minified code.
  */
