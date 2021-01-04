744b23b8c64adead5826e6c1c381ab3549c198df
Raoul v. R <vanruesc@users.noreply.github.com>
2021-01-04 23:42:04 +0100

    Add dist to gitignore

--- dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Plugin } from "esbuild";
-export interface GLSLOptions {
-    minify?: boolean;
-}
-export default function ({ minify }?: GLSLOptions): Plugin;
--- dist/types/minifyShader.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function minifyShader(source: string): string;
