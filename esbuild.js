0ec92754a7a40f8ab9648ee770b12101852f3189
Raoul v. R <vanruesc@users.noreply.github.com>
2022-03-24 13:00:11 +0100

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -4,7 +4,6 @@ import esbuild from "esbuild";
 const require = createRequire(import.meta.url);
 const pkg = require("./package");
 const date = (new Date()).toDateString();
-
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date}
  * ${pkg.homepage}
@@ -12,21 +11,24 @@ const banner = `/**
  * @license ${pkg.license}
  */`;
 
-const common = {
+await esbuild.build({
 	entryPoints: ["src/index.ts"],
+	outfile: `dist/${pkg.name}.js`,
 	banner: { js: banner },
 	platform: "node",
 	logLevel: "info",
+	format: "esm",
 	bundle: true,
 	minify: true
-};
+}).catch(() => process.exit(1));
 
-await esbuild.build(Object.assign({
-	outfile: `dist/${pkg.name}.js`,
-	format: "esm"
-}, common)).catch(() => process.exit(1));
-
-await esbuild.build(Object.assign({
+await esbuild.build({
+	entryPoints: ["src/index.ts"],
 	outfile: `dist/${pkg.name}.cjs`,
-	format: "cjs"
-}, common)).catch(() => process.exit(1));
+	banner: { js: banner },
+	platform: "node",
+	logLevel: "info",
+	format: "cjs",
+	bundle: true,
+	minify: true
+}).catch(() => process.exit(1));
