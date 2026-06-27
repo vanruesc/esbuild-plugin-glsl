08bf1d10b6d4c859bacb00984f77cff45aae869b
Raoul v. R. <vanruesc@outlook.de>
2026-06-27 17:00:49 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,14 @@
+import { defineConfig } from "eslint/config";
 import aether from "eslint-config-aether";
-export default [...aether];
+
+const eslintignore = {
+	ignores: [
+		"test/expected/**",
+		"test/generated/**"
+	]
+};
+
+export default defineConfig([
+	eslintignore,
+	aether
+]);
